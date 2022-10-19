a5778f7d7a8660d926c7b71d8df39e9fc80de331
kylepeavot <kylepeavot@oxwash.com>
2022-10-20 00:03:37 +0100

    Added TODOs

--- blog/src/routes/+page.svelte
+++ blog/src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ol>
+    <li>Splash page</li>
+    <li>Deployment of website?</li>
+    <li>Domain name for website</li>
+    <li>Blog stuff</li>
+    <li>Look into backend solutions (possibly svelte backend?)</li>
+</ol>
